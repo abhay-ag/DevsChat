0165ee7d518f059b8b1bf498b983796a4c6502c5
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-06-04 21:14:46 +0530

    Footer made completely

--- components/Footer.js
+++ components/Footer.js
@@ -1,16 +1,21 @@
 import { View, Text, StyleSheet, SafeAreaView } from "react-native";
-import React from "react";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-import Icon1 from 'react-native-vector-icons/Ionicons'
+import React, {useState} from "react";
+import Icon from "react-native-vector-icons/Ionicons";
+import Icon1 from 'react-native-vector-icons/FontAwesome'
 
 export default function Footer() {
+  const icons = [
+    'ios-home-outline', 'ios-search-outline', 'ios-add-circle-outline',
+    'ios-heart-outline', 
+  ]
   return (
     <View style={styles.footContainer}>
-      <Icon name="home" size={30} style={styles.icons} />
-      <Icon name="account-search" size={30} style={styles.icons} />
-      <Icon1 name="ios-add-circle-sharp" size={30} style={styles.icons} />
-      <Icon name="contactless-payment-circle" size={30} style={styles.icons} />
-      <Icon name="contactless-payment-circle" size={30} style={styles.icons} />
+      {
+        icons.map(icon => (
+          <Icon name = {icon} size = {30} style = {styles.icons} key = {icon} />
+        ))
+      }
+      <Icon1 name="user-o" size={25} style={styles.icons} />
     </View>
   );
 }
@@ -19,11 +24,13 @@ const styles = StyleSheet.create({
     position: "absolute",
     bottom: 0,
     height: 70,
-    backgroundColor: "black",
+    backgroundColor: "rgb(24,24,27)",
     flexDirection: "row",
     justifyContent: "space-around",
     width: "100%",
-    paddingTop: 15
+    paddingTop: 15,
+    borderTopWidth: 2,
+    borderColor: 'rgb(35,35,38)'
   },
   icons: {
     color: "white",
