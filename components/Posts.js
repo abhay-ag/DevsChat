aac871a584333b16f570387c9be7b170b0ad4094
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-06-05 17:39:52 +0530

    interaction for posts added

--- components/Posts.js
+++ components/Posts.js
@@ -1,41 +1,60 @@
 import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
 import React from "react";
+import Icon from "react-native-vector-icons/FontAwesome";
+import Icon1 from 'react-native-vector-icons/Octicons'
+import Icon2 from 'react-native-vector-icons/Feather'
 
-const windowWidth = Dimensions.get('window').width;
+const windowWidth = Dimensions.get("window").width;
 
-export default function Posts({userImg, userName}) {
+export default function Posts({ userImg, userName }) {
   return (
     <View style={styles.postContainer}>
       <View style={styles.userPost}>
-          <Image source={{uri: userImg}} style = {styles.userImg}/>
-          <Text style = {styles.userName}>{userName}</Text>
+        <Image source={{ uri: userImg }} style={styles.userImg} />
+        <Text style={styles.userName}>{userName}</Text>
       </View>
-      <Image source={{uri: userImg}} style={styles.postImage} />
+      <Image source={{ uri: userImg }} style={styles.postImage} />
+      <View style={styles.interaction}>
+        <Icon name="heart" size={24} style={styles.icons} />
+        <Icon1 name="comment" size={24} style = {styles.icons}/>
+        <Icon2 name="send" size={24} style = {styles.icons}/>
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
-  postContainer: {},
+  postContainer: {
+    marginVertical: 5,
+  },
   postImage: {
-    width: windowWidth ,
-    height: windowWidth ,
+    width: windowWidth,
+    height: windowWidth,
     marginVertical: 5,
   },
   userPost: {
     flexDirection: "row",
     paddingHorizontal: 10,
     paddingVertical: 5,
-    alignItems: 'center'
+    alignItems: "center",
   },
   userImg: {
-      width: 40,
-      height: 40,
-      borderRadius: 40/2
+    width: 40,
+    height: 40,
+    borderRadius: 40 / 2,
   },
   userName: {
-      color : 'white',
-      paddingHorizontal: 10,
-      fontWeight: '600',
-      fontSize: 17
+    color: "white",
+    paddingHorizontal: 10,
+    fontWeight: "600",
+    fontSize: 17,
+  },
+  icons: {
+    color: 'white',
+    marginHorizontal: 8,
+    marginVertical: 6
+  },
+  interaction: {
+    flexDirection: 'row',
+    alignItems: 'center',
   }
 });
