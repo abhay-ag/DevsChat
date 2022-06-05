43675350dc33d4346968ee37dbe99c017e615928
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-06-05 17:41:29 +0530

    posts added to the data | rendering start

--- data/users.js
+++ data/users.js
@@ -1,18 +1,57 @@
 export const data = [
-    {
-        name: 'abhay-ag',
-        pic: 'https://randomuser.me/api/portraits/men/1.jpg',
-    },{
-        name: 'v0nser',
-        pic: 'https://randomuser.me/api/portraits/men/2.jpg',
-    },{
-        name: 'abhi-cyber',
-        pic: 'https://randomuser.me/api/portraits/men/3.jpg',
-    },{
-        name: 'AbhishekM',
-        pic: 'https://randomuser.me/api/portraits/men/4.jpg',
-    },{
-        name: 'AbhishekT',
-        pic: 'https://randomuser.me/api/portraits/men/5.jpg',
-    },
-]
+  {
+    name: "abhay-ag",
+    pic: "https://randomuser.me/api/portraits/men/1.jpg",
+    posts: [
+      "https://randomuser.me/api/portraits/men/1.jpg",
+      "https://randomuser.me/api/portraits/men/2.jpg",
+      "https://randomuser.me/api/portraits/men/3.jpg",
+      "https://randomuser.me/api/portraits/men/4.jpg",
+      "https://randomuser.me/api/portraits/men/5.jpg",
+    ],
+  },
+  {
+    name: "v0nser",
+    pic: "https://randomuser.me/api/portraits/men/2.jpg",
+    posts: [
+        "https://randomuser.me/api/portraits/men/1.jpg",
+        "https://randomuser.me/api/portraits/men/2.jpg",
+        "https://randomuser.me/api/portraits/men/3.jpg",
+        "https://randomuser.me/api/portraits/men/4.jpg",
+        "https://randomuser.me/api/portraits/men/5.jpg",
+      ],
+  },
+  {
+    name: "abhi-cyber",
+    pic: "https://randomuser.me/api/portraits/men/3.jpg",
+    posts: [
+        "https://randomuser.me/api/portraits/men/1.jpg",
+        "https://randomuser.me/api/portraits/men/2.jpg",
+        "https://randomuser.me/api/portraits/men/3.jpg",
+        "https://randomuser.me/api/portraits/men/4.jpg",
+        "https://randomuser.me/api/portraits/men/5.jpg",
+      ],
+  },
+  {
+    name: "AbhishekM",
+    pic: "https://randomuser.me/api/portraits/men/4.jpg",
+    posts: [
+        "https://randomuser.me/api/portraits/men/1.jpg",
+        "https://randomuser.me/api/portraits/men/2.jpg",
+        "https://randomuser.me/api/portraits/men/3.jpg",
+        "https://randomuser.me/api/portraits/men/4.jpg",
+        "https://randomuser.me/api/portraits/men/5.jpg",
+      ],
+  },
+  {
+    name: "AbhishekT",
+    pic: "https://randomuser.me/api/portraits/men/5.jpg",
+    posts: [
+        "https://randomuser.me/api/portraits/men/1.jpg",
+        "https://randomuser.me/api/portraits/men/2.jpg",
+        "https://randomuser.me/api/portraits/men/3.jpg",
+        "https://randomuser.me/api/portraits/men/4.jpg",
+        "https://randomuser.me/api/portraits/men/5.jpg",
+      ],
+  },
+];
