c839c599d09cc539ce9a720048ffb4d41174e835
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-06-06 12:00:29 +0530

    Starting navigation || Components changed

--- App.js
+++ App.js
@@ -1,75 +1,22 @@
 import AppLoading from "expo-app-loading";
-import { useFonts } from "expo-font";
-import Footer from "./components/Footer";
-import { StyleSheet, Text, View } from "react-native";
-import Header from "./components/Header";
-import { FlatList } from "react-native";
-import { data } from "./data/users";
-import Posts from "./components/Posts";
-import Profile from "./components/Profile";
+import * as Font from "expo-font";
+import { useState } from "react";
+import Home from "./components/Home";
 
-let iterator = 0;
+const getFonts = () => Font.loadAsync({
+  Dancing_Script: require('./assets/fonts/Courgette.ttf')
+})
 
 export default function App() {
-  const profiles = ({ item }) => (
-    <Profile key={item.name} name={item.name} img={item.pic} />
-  );
-  const post = ({ item }) => {
-    if (iterator === 0) {
-      iterator++;
-      return (
-        <View>
-          <FlatList
-            horizontal={true}
-            data={data}
-            renderItem={profiles}
-            keyExtractor={(item) => item.name}
-            style={styles.flatList}
-            showsHorizontalScrollIndicator={false}
-          />
-          <Posts
-            userImg={item.pic}
-            userName={item.name}
-            userPosts={item.posts}
-          />
-        </View>
-      );
-    } else {
-      return (
-        <Posts userImg={item.pic} userName={item.name} userPosts={item.posts} />
-      );
-    }
-  };
-  let [fontsLoaded] = useFonts({
-    Dancing_Script: require("./assets/fonts/Courgette.ttf"),
-  });
-  if (!fontsLoaded) {
-    return <AppLoading />;
+  const [fontsLoaded, setFontsLoaded] = useState(false)
+  if (fontsLoaded) {
+    return(<Home />)
   }
-  return (
-    <View style={styles.container}>
-      <Header />
-      <FlatList
-        data={data}
-        renderItem={post}
-        keyExtractor={(item) => item.name}
-        showsVerticalScrollIndicator = {false}
-      />
-      <Footer />
-    </View>
-  );
+  else {
+    return <AppLoading 
+      startAsync={getFonts}
+      onFinish = {() => setFontsLoaded(true)}
+      onError = {() => console.warn("")}
+    />;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    position: "relative",
-    backgroundColor: "rgb(10,15,20)",
-  },
-  flatList: {
-    backgroundColor: "rgb(10,15,20)",
-    paddingVertical: 4,
-    borderBottomWidth: 1,
-    borderColor: "rgba(255,255,255,0.1)",
-  },
-});
---
+++ components/Home.js
@@ -0,0 +1,69 @@
+import { FlatList } from "react-native";
+import { data } from "../data/users";
+import Posts from "./Posts";
+import Profile from "./Profile";
+import { View, Text } from 'react-native'
+import React from 'react'
+import { StyleSheet } from "react-native";
+import Footer from "./Footer";
+import Header from "./Header";
+
+let iterator = 0;
+
+export default function Home() {
+  const profiles = ({ item }) => (
+    <Profile key={item.name} name={item.name} img={item.pic} />
+  );
+  const post = ({ item }) => {
+    if (iterator === 0) {
+      iterator++;
+      return (
+        <View>
+          <FlatList
+            horizontal={true}
+            data={data}
+            renderItem={profiles}
+            keyExtractor={(item) => item.name}
+            style={styles.flatList}
+            showsHorizontalScrollIndicator={false}
+          />
+          <Posts
+            userImg={item.pic}
+            userName={item.name}
+            userPosts={item.posts}
+          />
+        </View>
+      );
+    } else {
+      return (
+        <Posts userImg={item.pic} userName={item.name} userPosts={item.posts} />
+      );
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <Header />
+      <FlatList
+        data={data}
+        renderItem={post}
+        keyExtractor={(item) => item.name}
+        showsVerticalScrollIndicator = {false}
+      />
+      <Footer />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: "relative",
+    backgroundColor: "rgb(10,15,20)",
+  },
+  flatList: {
+    backgroundColor: "rgb(10,15,20)",
+    paddingVertical: 4,
+    borderBottomWidth: 1,
+    borderColor: "rgba(255,255,255,0.1)",
+  },
+});
