48b5ba04dc345d1a4805228b0b9302488e2c30bd
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-06-08 14:09:28 +0530

    Chat Box made

--- components/Chat.js
+++ components/Chat.js
@@ -1,12 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import Header from "./Header";
+import Type from "./components/Type";
 
 export default function Chat(props) {
   const { name, userName } = props.route.params
   return (
-    <View>
+    <View style = {styles.container}>
       <Header showBack={true} userName = {userName}/>
+      <Type name = {userName}/>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgb(10,15,20)',
+    position: 'relative'
+  }
+})
---
+++ components/components/Type.js
@@ -0,0 +1,33 @@
+import { View, Text, StyleSheet, TextInput, SafeAreaView } from "react-native";
+import React from "react";
+
+export default function Type(props) {
+  return (
+    <View style={styles.chat}>
+      <TextInput
+        style={styles.message}
+        placeholder={`Type a message @${props.name}`}
+        placeholderTextColor={"rgba(255,255,255,0.2)"}
+        autoCapitalize = {'none'}
+        autoCorrect = {false}
+        autoComplete = {false}
+      />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  chat: {
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    width: "100%",
+},
+message: {
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    backgroundColor: "rgba(255,255,255,0.1)",
+    width: "100%",
+    fontSize: 20,
+    color: "rgb(255,255,255)",
+  },
+});
