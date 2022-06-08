aab1c3b2de0f870a16eaa24c824acb0c2e8771b8
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-06-08 20:22:12 +0530

    basic layout made

--- components/Login.js
+++ components/Login.js
@@ -4,13 +4,17 @@ import {
   StyleSheet,
   TextInput,
   KeyboardAvoidingView,
+  TouchableOpacity,
 } from "react-native";
 import React from "react";
 import { data } from "../data/users";
 
 export default function Login() {
   return (
-    <KeyboardAvoidingView style={styles.container}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
       <View style={styles.innerContainer}>
         <Text style={styles.text}>DevsChat</Text>
         <TextInput
@@ -18,6 +22,9 @@ export default function Login() {
           placeholder="Enter user name"
           placeholderTextColor={"rgba(255,255,255,0.16)"}
         />
+        <TouchableOpacity style={styles.cusBut}>
+          <Text style={styles.button}>Login</Text>
+        </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
   );
@@ -29,7 +36,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     flex: 1,
     backgroundColor: "rgb(10,15,20)",
-    flexDirection: 'row'
+    flexDirection: "row",
   },
   input: {
     width: "100%",
@@ -47,8 +54,27 @@ const styles = StyleSheet.create({
     fontSize: 50,
   },
   innerContainer: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent:'center',
-  }
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    flex: 1,
+    color: "white",
+    fontWeight: "700",
+    backgroundColor: "#3b82f6",
+    fontSize: 23,
+    textAlign: "center",
+    borderRadius: 15,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  cusBut: {
+    flexDirection: "row",
+    marginTop: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden",
+    borderRadius: 10,
+  },
 });
