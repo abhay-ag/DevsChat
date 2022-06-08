6162886b6414f19c98d584b4dc74f8a007d1c1c2
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-06-08 14:19:26 +0530

    Added scroll up when keyboard active

--- components/Chat.js
+++ components/Chat.js
@@ -2,20 +2,34 @@ import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import Header from "./Header";
 import Type from "./components/Type";
+import { KeyboardAvoidingView } from "react-native";
 
 export default function Chat(props) {
-  const { name, userName } = props.route.params
+  const { name, userName } = props.route.params;
   return (
-    <View style = {styles.container}>
-      <Header showBack={true} userName = {userName}/>
-      <Type name = {userName}/>
-    </View>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <Header showBack={true} userName={userName} />
+      <View style = {styles.innerContainer}>
+        <View style={styles.chatContainer}></View>
+        <Type name={userName} />
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'rgb(10,15,20)',
-    position: 'relative'
+    backgroundColor: "rgb(10,15,20)",
+    position: "relative",
+  },
+  chatContainer: {
+    flex: 1,
+  },
+  innerContainer: {
+    paddingBottom: 15,
+    flex: 1
   }
-})
+});
