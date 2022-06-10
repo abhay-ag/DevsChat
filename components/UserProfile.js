26f4fe02df47b94cb3e2887e225da20f7a8e24ca
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-06-10 20:57:03 +0530

    profile page header made | layout planning

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,15 +1,20 @@
-import { View, Text, StyleSheet, StatusBar } from "react-native";
+import { View, Text, StyleSheet, StatusBar, SafeAreaView } from "react-native";
 import React from "react";
 import Footer from "./Footer";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function UserProfile(props) {
   const { userName } = props.route.params;
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor={"rgb(10,15,20)"} />
-      <View style={styles.view}>
-        <Text>{userName}</Text>
-      </View>
+      <SafeAreaView>
+        <View style={styles.header}>
+          <Text style={styles.uName}>{userName}</Text>
+          <Icon name="angle-down" size={30} style={styles.icon} />
+        </View>
+      </SafeAreaView>
+      <View style={styles.view}></View>
       <Footer user={userName} />
     </View>
   );
@@ -21,5 +26,23 @@ const styles = StyleSheet.create({
   },
   view: {
     flex: 1,
+  },
+  header: {
+    marginVertical: 5,
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(255,255,255,0.1)',
+    paddingVertical: 10
+  },
+  uName: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "800",
+  },
+  icon: {
+    color: "white",
+    marginHorizontal: 20
   },
 });
