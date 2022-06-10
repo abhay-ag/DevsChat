b30d4f0624c80d922cbf2d44b9f336e128bcd0f9
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-06-10 14:50:59 +0530

    styling of the profile page

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,11 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import Footer from "./Footer";
 
 export default function UserProfile(props) {
   const { user } = props.route.params;
   return (
-    <View>
+    <View style={styles.container}>
       <Text>{user}</Text>
+      <Footer />
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgb(10,15,20)",
+  },
+});
